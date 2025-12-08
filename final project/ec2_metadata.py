895740d4ff45f462e40f8fe8e908756505ea01c4
harshgdave <hdave2@umbc.edu>
2025-12-08 17:17:25 +0530

    Delete final project/ec2_metadata.py

--- final project/ec2_metadata.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-metadata_url = "http://169.254.169.254/latest/meta-data/"
-
-instance_id = requests.get(metadata_url + "instance-id").text
-public_ip = requests.get(metadata_url + "public-ipv4").text
-
-print("Instance ID:", instance_id)
-print("Public IP:", public_ip)
